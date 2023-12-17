af030cfabedc14adccd690099a2bca6eac9e42cd
RLAlpha49 <75044176+RLAlpha49@users.noreply.github.com>
2023-12-16 19:52:05 -0600

    Error handling for rate limit

--- test.js
+++ test.js
@@ -4,53 +4,63 @@ const app = express();
 const queries = require('./queries');
 
 async function fetchAniListData(username) {
-    const userIdResponse = await axios.post('https://graphql.anilist.co', {
-        query: queries.USER_ID,
-        variables: { userName: username },
-    });
+    try {
+        const userIdResponse = await axios.post('https://graphql.anilist.co', {
+            query: queries.USER_ID,
+            variables: { userName: username },
+        });
 
-    const userId = userIdResponse.data.data.User.id;
+        const userId = userIdResponse.data.data.User.id;
 
-    const [animeCountResponse, totalFollowersResponse, totalFollowingResponse, mangaCountResponse, chaptersReadResponse, episodesWatchedResponse, minutesWatchedResponse] = await Promise.all([
-        axios.post('https://graphql.anilist.co', {
-            query: queries.USER_ANIME_COUNT,
-            variables: { userName: username },
-        }),
-        axios.post('https://graphql.anilist.co', {
-            query: queries.USER_TOTAL_FOLLOWERS,
-            variables: { userId },
-        }),
-        axios.post('https://graphql.anilist.co', {
-            query: queries.USER_TOTAL_FOLLOWING,
-            variables: { userId },
-        }),
-        axios.post('https://graphql.anilist.co', {
-            query: queries.USER_MANGA_COUNT,
-            variables: { userName: username },
-        }),
-        axios.post('https://graphql.anilist.co', {
-            query: queries.USER_CHAPTERS_READ,
-            variables: { userName: username },
-        }),
-        axios.post('https://graphql.anilist.co', {
-            query: queries.USER_EPISODES_WATCHED,
-            variables: { userName: username },
-        }),
-        axios.post('https://graphql.anilist.co', {
-            query: queries.USER_HOURS_WATCHED,
-            variables: { userName: username },
-        }),
-    ]);
+        const [animeCountResponse, totalFollowersResponse, totalFollowingResponse, mangaCountResponse, chaptersReadResponse, episodesWatchedResponse, minutesWatchedResponse] = await Promise.all([
+            axios.post('https://graphql.anilist.co', {
+                query: queries.USER_ANIME_COUNT,
+                variables: { userName: username },
+            }),
+            axios.post('https://graphql.anilist.co', {
+                query: queries.USER_TOTAL_FOLLOWERS,
+                variables: { userId },
+            }),
+            axios.post('https://graphql.anilist.co', {
+                query: queries.USER_TOTAL_FOLLOWING,
+                variables: { userId },
+            }),
+            axios.post('https://graphql.anilist.co', {
+                query: queries.USER_MANGA_COUNT,
+                variables: { userName: username },
+            }),
+            axios.post('https://graphql.anilist.co', {
+                query: queries.USER_CHAPTERS_READ,
+                variables: { userName: username },
+            }),
+            axios.post('https://graphql.anilist.co', {
+                query: queries.USER_EPISODES_WATCHED,
+                variables: { userName: username },
+            }),
+            axios.post('https://graphql.anilist.co', {
+                query: queries.USER_HOURS_WATCHED,
+                variables: { userName: username },
+            }),
+        ]);
 
-    return {
-        animeCount: animeCountResponse.data.data.User.statistics.anime.count,
-        totalFollowers: totalFollowersResponse.data.data.Page.pageInfo.total,
-        totalFollowing: totalFollowingResponse.data.data.Page.pageInfo.total,
-        mangaCount: mangaCountResponse.data.data.User.statistics.manga.count,
-        chaptersRead: chaptersReadResponse.data.data.User.statistics.manga.chaptersRead,
-        episodesWatched: episodesWatchedResponse.data.data.User.statistics.anime.episodesWatched,
-        hoursWatched: minutesWatchedResponse.data.data.User.statistics.anime.minutesWatched / 60,
-    };
+        return {
+            animeCount: animeCountResponse.data.data.User.statistics.anime.count,
+            totalFollowers: totalFollowersResponse.data.data.Page.pageInfo.total,
+            totalFollowing: totalFollowingResponse.data.data.Page.pageInfo.total,
+            mangaCount: mangaCountResponse.data.data.User.statistics.manga.count,
+            chaptersRead: chaptersReadResponse.data.data.User.statistics.manga.chaptersRead,
+            episodesWatched: episodesWatchedResponse.data.data.User.statistics.anime.episodesWatched,
+            hoursWatched: Math.round(minutesWatchedResponse.data.data.User.statistics.anime.minutesWatched / 60),
+        };
+    } catch (error) {
+        if (error.response && error.response.status === 429) {
+            console.error('Rate limit exceeded');
+            return null;
+        } else {
+            console.error(error);
+            return null;
+        }
+    }
 }
 
 // Generate SVG
@@ -91,19 +101,26 @@ function generateTotalFollowingSVG(data) {
 
 app.get('/:username', async (req, res) => {
     const data = await fetchAniListData(req.params.username);
+    if (data === null) {
+        res.status(429).send('Rate limit exceeded');
+        return;
+    }
+
     res.setHeader('Content-Type', 'image/svg+xml');
     const svg = `
         <svg xmlns="http://www.w3.org/2000/svg" width="500" height="700">
             ${generateAnimeCountSVG(data)}
-            ${generateEpisodesWatchedSVG(data)}
-            ${generateHoursWatchedSVG(data)}
-            ${generateMangaCountSVG(data)}
-            ${generateChaptersReadSVG(data)}
             ${generateTotalFollowersSVG(data)}
             ${generateTotalFollowingSVG(data)}
+            ${generateMangaCountSVG(data)}
+            ${generateChaptersReadSVG(data)}
+            ${generateEpisodesWatchedSVG(data)}
+            ${generateHoursWatchedSVG(data)}
         </svg>
     `;
     res.send(svg);
 });
 
-app.listen(3000, () => console.log('Server running on port 3000'));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
