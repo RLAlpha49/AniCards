e9d64df6876468aa806d025cd5fda3963c3b3712
RLAlpha49 <75044176+RLAlpha49@users.noreply.github.com>
2023-12-16 19:36:01 -0600

    Added more data

--- queries.js
+++ queries.js
@@ -19,6 +19,50 @@ module.exports = {
             } 
         }
     `,
+    USER_EPISODES_WATCHED: `
+    query ($userName: String) { 
+        User(name: $userName) { 
+            statistics { 
+                anime { 
+                    episodesWatched 
+                } 
+            } 
+        } 
+    }
+    `,
+    USER_HOURS_WATCHED: `
+        query ($userName: String) { 
+            User(name: $userName) { 
+                statistics { 
+                    anime { 
+                        minutesWatched 
+                    } 
+                } 
+            } 
+        }
+    `,
+    USER_MANGA_COUNT: `
+    query ($userName: String) { 
+        User(name: $userName) { 
+            statistics { 
+                manga { 
+                    count 
+                } 
+            } 
+        } 
+    }
+    `,
+    USER_CHAPTERS_READ: `
+        query ($userName: String) { 
+            User(name: $userName) { 
+                statistics { 
+                    manga { 
+                        chaptersRead 
+                    } 
+                } 
+            } 
+        }
+    `,
     USER_TOTAL_ACTIVITIES: `
         query ($userName: String) {
             User(name: $userName) { 
@@ -33,6 +77,9 @@ module.exports = {
     USER_TOTAL_FOLLOWERS: `
         query ($userId: Int!) { 
             Page {
+                pageInfo {
+                    total
+                }
                 followers(userId: $userId) {
                     id
                 }
@@ -42,6 +89,9 @@ module.exports = {
     USER_TOTAL_FOLLOWING: `
         query ($userId: Int!) { 
             Page {
+                pageInfo {
+                    total
+                }
                 following(userId: $userId) {
                     id
                 }
--- test.js
+++ test.js
@@ -1,49 +1,108 @@
 const axios = require('axios');
 const express = require('express');
 const app = express();
-const { USER_ID, USER_ANIME_COUNT, USER_TOTAL_FOLLOWERS, USER_TOTAL_FOLLOWING } = require('./queries');
+const queries = require('./queries');
 
-// Fetch data from AniList's API
 async function fetchAniListData(username) {
     const userIdResponse = await axios.post('https://graphql.anilist.co', {
-        query: USER_ID,
+        query: queries.USER_ID,
         variables: { userName: username },
     });
+
     const userId = userIdResponse.data.data.User.id;
 
-    const animeCountResponse = await axios.post('https://graphql.anilist.co', {
-        query: USER_ANIME_COUNT,
-        variables: { userName: username },
-    });
-    const animeCount = animeCountResponse.data.data.User.statistics.anime.count;
+    const [animeCountResponse, totalFollowersResponse, totalFollowingResponse, mangaCountResponse, chaptersReadResponse, episodesWatchedResponse, minutesWatchedResponse] = await Promise.all([
+        axios.post('https://graphql.anilist.co', {
+            query: queries.USER_ANIME_COUNT,
+            variables: { userName: username },
+        }),
+        axios.post('https://graphql.anilist.co', {
+            query: queries.USER_TOTAL_FOLLOWERS,
+            variables: { userId },
+        }),
+        axios.post('https://graphql.anilist.co', {
+            query: queries.USER_TOTAL_FOLLOWING,
+            variables: { userId },
+        }),
+        axios.post('https://graphql.anilist.co', {
+            query: queries.USER_MANGA_COUNT,
+            variables: { userName: username },
+        }),
+        axios.post('https://graphql.anilist.co', {
+            query: queries.USER_CHAPTERS_READ,
+            variables: { userName: username },
+        }),
+        axios.post('https://graphql.anilist.co', {
+            query: queries.USER_EPISODES_WATCHED,
+            variables: { userName: username },
+        }),
+        axios.post('https://graphql.anilist.co', {
+            query: queries.USER_HOURS_WATCHED,
+            variables: { userName: username },
+        }),
+    ]);
 
-    const totalFollowersResponse = await axios.post('https://graphql.anilist.co', {
-        query: USER_TOTAL_FOLLOWERS,
-        variables: { userId: userId },
-    });
-    const totalFollowers = totalFollowersResponse.data.data.Page.followers.length;
-
-    const totalFollowingResponse = await axios.post('https://graphql.anilist.co', {
-        query: USER_TOTAL_FOLLOWING,
-        variables: { userId: userId },
-    });
-    const totalFollowing = totalFollowingResponse.data.data.Page.following.length;
-
-    return { animeCount, totalFollowers, totalFollowing };
+    return {
+        animeCount: animeCountResponse.data.data.User.statistics.anime.count,
+        totalFollowers: totalFollowersResponse.data.data.Page.pageInfo.total,
+        totalFollowing: totalFollowingResponse.data.data.Page.pageInfo.total,
+        mangaCount: mangaCountResponse.data.data.User.statistics.manga.count,
+        chaptersRead: chaptersReadResponse.data.data.User.statistics.manga.chaptersRead,
+        episodesWatched: episodesWatchedResponse.data.data.User.statistics.anime.episodesWatched,
+        hoursWatched: minutesWatchedResponse.data.data.User.statistics.anime.minutesWatched / 60,
+    };
 }
 
 // Generate SVG
-function generateSVG(count) {
-    return `<svg xmlns="http://www.w3.org/2000/svg" width="1000" height="100">
-        <text x="0" y="50" font-size="35">Watched anime count: ${count}</text>
-    </svg>`;
+function generateSVG(label, value, y) {
+    return `
+        <text x="0" y="${y}" font-size="35">${label}: ${value}</text>
+    `;
 }
 
 // Setup server
+function generateAnimeCountSVG(data) {
+    return generateSVG('Watched anime count', data.animeCount, 50);
+}
+
+function generateEpisodesWatchedSVG(data) {
+    return generateSVG('Total episodes watched', data.episodesWatched, 550);
+}
+
+function generateHoursWatchedSVG(data) {
+    return generateSVG('Total hours watched', data.hoursWatched, 650);
+}
+
+function generateMangaCountSVG(data) {
+    return generateSVG('Total manga read', data.mangaCount, 350);
+}
+
+function generateChaptersReadSVG(data) {
+    return generateSVG('Total chapters read', data.chaptersRead, 450);
+}
+
+function generateTotalFollowersSVG(data) {
+    return generateSVG('Total followers', data.totalFollowers, 150);
+}
+
+function generateTotalFollowingSVG(data) {
+    return generateSVG('Total following', data.totalFollowing, 250);
+}
+
 app.get('/:username', async (req, res) => {
-    const count = await fetchAniListData(req.params.username);
-    const svg = generateSVG(count);
+    const data = await fetchAniListData(req.params.username);
     res.setHeader('Content-Type', 'image/svg+xml');
+    const svg = `
+        <svg xmlns="http://www.w3.org/2000/svg" width="500" height="700">
+            ${generateAnimeCountSVG(data)}
+            ${generateEpisodesWatchedSVG(data)}
+            ${generateHoursWatchedSVG(data)}
+            ${generateMangaCountSVG(data)}
+            ${generateChaptersReadSVG(data)}
+            ${generateTotalFollowersSVG(data)}
+            ${generateTotalFollowingSVG(data)}
+        </svg>
+    `;
     res.send(svg);
 });
 
