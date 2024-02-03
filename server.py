7de275de7e5e851198bb07565d49246bda3c7d0f
RLAlpha49 <75044176+RLAlpha49@users.noreply.github.com>
2024-02-03 15:59:46 -0600

    Add file opening functionality

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 # Used to run server through waitress
 import subprocess
 import os
+import glob
 
 def run_server():
     return subprocess.Popen(['waitress-serve', '--port=5000', '--threads=4', 'main:app'])
@@ -11,9 +12,17 @@ def restart_server(server_process):
     os.system('git pull')
     return run_server()
 
+def open_newest_file(file_type):
+    files = glob.glob(f'{file_type}_*.log')
+    newest_file = max(files, key=os.path.getctime)
+    os.startfile(newest_file)
+
 server_process = run_server()
 
 while True:
     command = input()
     if command == 'restart':
         server_process = restart_server(server_process)
+    elif command.startswith('open '):
+        file_type = command.split(' ')[1]
+        open_newest_file(file_type)
