c0add136fd8853adb69ed9775877c8c733659e91
RLAlpha49 <75044176+RLAlpha49@users.noreply.github.com>
2024-02-07 10:25:05 -0600

    Update pylint workflow, requirements, and wrapper script

--- wrapper.py
+++ wrapper.py
@@ -1,16 +1,24 @@
-# wrapper.py
+"""
+This module continuously runs a subprocess (server.py) and restarts it if it exits with a special status code.
+"""
+
+import os
 import subprocess
 import time
 
+# Get the directory of the current script
+script_dir = os.path.dirname(os.path.realpath(__file__))
+
 while True:
     try:
-        process = subprocess.Popen(['python', 'server.py'])
-        process.wait()
+        # Use 'with' statement for resource-allocating operation
+        with subprocess.Popen(['python', os.path.join(script_dir, 'server.py')]) as process:
+            process.wait()
 
-        # Check if server.py has exited with a special status code
-        if process.returncode != 2:
-            break
-    except Exception as e:
+            # Check if server.py has exited with a special status code
+            if process.returncode != 2:
+                break
+    except subprocess.SubprocessError as e:  # Catch a more specific exception
         print(f"An error occurred: {e}")
         input("Press Enter to restart the server...")
 
