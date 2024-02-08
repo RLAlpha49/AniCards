5f2d054c0a0b178dd07be61966a44de8425f3650
RLAlpha49 <75044176+RLAlpha49@users.noreply.github.com>
2024-02-07 20:56:32 -0600

    Refractored & Formatted AniListData.py

--- AniListData.py
+++ AniListData.py
@@ -9,15 +9,224 @@ import queries
 from logger import log_message
 
 
-# Function to fetch data from AniList for a given username
+def fetch_anime_data(response_data, keys):
+    """
+    Extracts anime data from the response data.
+
+    The function extracts various statistics about the user's anime watching habits,
+    as well as information about their favorite genres, tags, voice actors, studios, and staff.
+
+    Parameters:
+    response_data (dict): The response data from the AniList API.
+    keys (list): The keys to extract from the response data.
+
+    Returns:
+    dict: A dictionary containing the extracted data.
+
+    Raises:
+    DataExtractionError: If a key is missing in the response data.
+    """
+    data = {}
+    try:
+        # Extract the data for each SVG
+        data["animeStats"] = {
+            "count": response_data["data"]["User"]["statistics"]["anime"]["count"],
+            "episodesWatched": response_data["data"]["User"]["statistics"]["anime"][
+                "episodesWatched"
+            ],
+            "minutesWatched": response_data["data"]["User"]["statistics"]["anime"][
+                "minutesWatched"
+            ],
+            "meanScore": response_data["data"]["User"]["statistics"]["anime"][
+                "meanScore"
+            ],
+            "standardDeviation": response_data["data"]["User"]["statistics"]["anime"][
+                "standardDeviation"
+            ],
+        }
+        if "animeGenres" in keys:
+            top_genres = sorted(
+                response_data["data"]["User"]["statistics"]["anime"]["genres"],
+                key=lambda x: x["count"],
+                reverse=True,
+            )[:5]
+            data["animeGenres"] = top_genres
+
+        if "animeTags" in keys:
+            tags = response_data["data"]["User"]["statistics"]["anime"]["tags"]
+            top_tags = sorted(tags, key=lambda x: x["count"], reverse=True)[:5]
+            top_tags = [
+                {"tag": tag["tag"]["name"], "count": tag["count"]} for tag in top_tags
+            ]
+            data["animeTags"] = top_tags
+
+        if "animeVoiceActors" in keys:
+            voice_actors = response_data["data"]["User"]["statistics"]["anime"][
+                "voiceActors"
+            ]
+            top_voice_actors = sorted(
+                voice_actors, key=lambda x: x["count"], reverse=True
+            )[:5]
+            top_voice_actors = [
+                {
+                    "voiceActor": actor["voiceActor"]["name"]["full"],
+                    "count": actor["count"],
+                }
+                for actor in top_voice_actors
+            ]
+            data["animeVoiceActors"] = top_voice_actors
+
+        if "animeStudios" in keys:
+            studios = response_data["data"]["User"]["statistics"]["anime"]["studios"]
+            top_studios = sorted(studios, key=lambda x: x["count"], reverse=True)[:5]
+            top_studios = [
+                {"studio": studio["studio"]["name"], "count": studio["count"]}
+                for studio in top_studios
+            ]
+            data["animeStudios"] = top_studios
+
+        if "animeStaff" in keys:
+            staff = response_data["data"]["User"]["statistics"]["anime"]["staff"]
+            top_staff = sorted(staff, key=lambda x: x["count"], reverse=True)[:5]
+            top_staff = [
+                {
+                    "staff": staff_member["staff"]["name"]["full"],
+                    "count": staff_member["count"],
+                }
+                for staff_member in top_staff
+            ]
+            data["animeStaff"] = top_staff
+    except KeyError as e:
+        raise ValueError(f"Missing key {e} in anime response data") from e
+    return data
+
+
+def fetch_manga_data(response_data, keys):
+    """
+    Extracts manga data from the response data.
+
+    The function extracts various statistics about the user's manga reading habits,
+    as well as information about their favorite genres, tags, and staff.
+
+    Parameters:
+    response_data (dict): The response data from the AniList API.
+    keys (list): The keys to extract from the response data.
+
+    Returns:
+    dict: A dictionary containing the extracted data.
+
+    Raises:
+    ValueError: If a key is missing in the response data.
+    """
+    data = {}
+    try:
+        # Extract the data for each SVG
+        data["mangaStats"] = {
+            "count": response_data["data"]["User"]["statistics"]["manga"]["count"],
+            "chaptersRead": response_data["data"]["User"]["statistics"]["manga"][
+                "chaptersRead"
+            ],
+            "volumesRead": response_data["data"]["User"]["statistics"]["manga"][
+                "volumesRead"
+            ],
+            "meanScore": response_data["data"]["User"]["statistics"]["manga"][
+                "meanScore"
+            ],
+            "standardDeviation": response_data["data"]["User"]["statistics"]["manga"][
+                "standardDeviation"
+            ],
+        }
+        if "mangaGenres" in keys:
+            genres = response_data["data"]["User"]["statistics"]["manga"]["genres"]
+            top_genres = sorted(genres, key=lambda x: x["count"], reverse=True)[:5]
+            top_genres = [
+                {"genre": genre["genre"], "count": genre["count"]}
+                for genre in top_genres
+            ]
+            data["mangaGenres"] = top_genres
+
+        if "mangaTags" in keys:
+            tags = response_data["data"]["User"]["statistics"]["manga"]["tags"]
+            top_tags = sorted(tags, key=lambda x: x["count"], reverse=True)[:5]
+            top_tags = [
+                {"tag": tag["tag"]["name"], "count": tag["count"]} for tag in top_tags
+            ]
+            data["mangaTags"] = top_tags
+
+        if "mangaStaff" in keys:
+            staff = response_data["data"]["User"]["statistics"]["manga"]["staff"]
+            top_staff = sorted(staff, key=lambda x: x["count"], reverse=True)[:5]
+            top_staff = [
+                {
+                    "staff": staff_member["staff"]["name"]["full"],
+                    "count": staff_member["count"],
+                }
+                for staff_member in top_staff
+            ]
+            data["mangaStaff"] = top_staff
+    except KeyError as e:
+        raise ValueError(f"Missing key {e} in manga response data") from e
+    return data
+
+
+def fetch_social_data(response_data):
+    """
+    Extracts social data from the response data.
+
+    The function extracts various statistics about the user's social activity,
+    such as the total number of followers, following, activities, thread posts,
+    comments, and reviews.
+
+    Parameters:
+    response_data (dict): The response data from the AniList API.
+
+    Returns:
+    dict: A dictionary containing the extracted data.
+
+    Raises:
+    ValueError: If a key is missing in the response data.
+    """
+    data = {}
+    try:
+        # Extract social stats
+        data["socialStats"] = {
+            "totalFollowers": response_data["data"]["followersPage"]["pageInfo"][
+                "total"
+            ],
+            "totalFollowing": response_data["data"]["followingPage"]["pageInfo"][
+                "total"
+            ],
+            "totalActivity": sum(
+                amount["amount"]
+                for amount in response_data["data"]["User"]["stats"]["activityHistory"]
+            ),
+            "threadPostsCommentsCount": response_data["data"]["threadsPage"][
+                "pageInfo"
+            ]["total"]
+            + response_data["data"]["threadCommentsPage"]["pageInfo"]["total"],
+            "totalReviews": response_data["data"]["reviewsPage"]["pageInfo"]["total"],
+        }
+    except KeyError as e:
+        raise ValueError(f"Missing key {e} in social response data") from e
+    return data
+
+
 def fetch_anilist_data(username, keys):
     """
-    Fetches data for a given user from AniList. The data fetched depends on the keys provided.
+    Fetches data from AniList for a specific user.
+
+    The function sends a request to the AniList API to fetch data for the specified user.
+    It then extracts the desired data from the response using the provided keys.
+
     Parameters:
     username (str): The username of the AniList user.
-    keys (list): The list of keys indicating the type of data to fetch.
+    keys (list): The keys to extract from the response data.
+
     Returns:
-    dict: A dictionary containing the fetched data. If an error occurs, returns None.
+    dict: A dictionary containing the extracted data, or None if an error occurred.
+
+    Raises:
+    requests.exceptions.RequestException: If a network error occurred.
     """
     try:
         log_message(f"Started fetching data for {username}", "debug")
@@ -54,155 +263,19 @@ def fetch_anilist_data(username, keys):
             if response_data is None or "data" not in response_data:
                 log_message("Error: No data in response", "error")
                 return None
-            # Extract the data for each SVG
-            data["animeStats"] = {
-                "count": response_data["data"]["User"]["statistics"]["anime"]["count"],
-                "episodesWatched": response_data["data"]["User"]["statistics"]["anime"][
-                    "episodesWatched"
-                ],
-                "minutesWatched": response_data["data"]["User"]["statistics"]["anime"][
-                    "minutesWatched"
-                ],
-                "meanScore": response_data["data"]["User"]["statistics"]["anime"][
-                    "meanScore"
-                ],
-                "standardDeviation": response_data["data"]["User"]["statistics"][
-                    "anime"
-                ]["standardDeviation"],
-            }
-            if "animeGenres" in keys:
-                top_genres = sorted(
-                    response_data["data"]["User"]["statistics"]["anime"]["genres"],
-                    key=lambda x: x["count"],
-                    reverse=True,
-                )[:5]
-                data["animeGenres"] = top_genres
 
-            if "animeTags" in keys:
-                tags = response_data["data"]["User"]["statistics"]["anime"]["tags"]
-                top_tags = sorted(tags, key=lambda x: x["count"], reverse=True)[:5]
-                top_tags = [
-                    {"tag": tag["tag"]["name"], "count": tag["count"]}
-                    for tag in top_tags
-                ]
-                data["animeTags"] = top_tags
-
-            if "animeVoiceActors" in keys:
-                voice_actors = response_data["data"]["User"]["statistics"]["anime"][
-                    "voiceActors"
-                ]
-                top_voice_actors = sorted(
-                    voice_actors, key=lambda x: x["count"], reverse=True
-                )[:5]
-                top_voice_actors = [
-                    {
-                        "voiceActor": actor["voiceActor"]["name"]["full"],
-                        "count": actor["count"],
-                    }
-                    for actor in top_voice_actors
-                ]
-                data["animeVoiceActors"] = top_voice_actors
-
-            if "animeStudios" in keys:
-                studios = response_data["data"]["User"]["statistics"]["anime"][
-                    "studios"
-                ]
-                top_studios = sorted(studios, key=lambda x: x["count"], reverse=True)[
-                    :5
-                ]
-                top_studios = [
-                    {"studio": studio["studio"]["name"], "count": studio["count"]}
-                    for studio in top_studios
-                ]
-                data["animeStudios"] = top_studios
-
-            if "animeStaff" in keys:
-                staff = response_data["data"]["User"]["statistics"]["anime"]["staff"]
-                top_staff = sorted(staff, key=lambda x: x["count"], reverse=True)[:5]
-                top_staff = [
-                    {
-                        "staff": staff_member["staff"]["name"]["full"],
-                        "count": staff_member["count"],
-                    }
-                    for staff_member in top_staff
-                ]
-                data["animeStaff"] = top_staff
-
-            # Extract the data for each SVG
-            data["mangaStats"] = {
-                "count": response_data["data"]["User"]["statistics"]["manga"]["count"],
-                "chaptersRead": response_data["data"]["User"]["statistics"]["manga"][
-                    "chaptersRead"
-                ],
-                "volumesRead": response_data["data"]["User"]["statistics"]["manga"][
-                    "volumesRead"
-                ],
-                "meanScore": response_data["data"]["User"]["statistics"]["manga"][
-                    "meanScore"
-                ],
-                "standardDeviation": response_data["data"]["User"]["statistics"][
-                    "manga"
-                ]["standardDeviation"],
-            }
-            if "mangaGenres" in keys:
-                genres = response_data["data"]["User"]["statistics"]["manga"]["genres"]
-                top_genres = sorted(genres, key=lambda x: x["count"], reverse=True)[:5]
-                top_genres = [
-                    {"genre": genre["genre"], "count": genre["count"]}
-                    for genre in top_genres
-                ]
-                data["mangaGenres"] = top_genres
-
-            if "mangaTags" in keys:
-                tags = response_data["data"]["User"]["statistics"]["manga"]["tags"]
-                top_tags = sorted(tags, key=lambda x: x["count"], reverse=True)[:5]
-                top_tags = [
-                    {"tag": tag["tag"]["name"], "count": tag["count"]}
-                    for tag in top_tags
-                ]
-                data["mangaTags"] = top_tags
-
-            if "mangaStaff" in keys:
-                staff = response_data["data"]["User"]["statistics"]["manga"]["staff"]
-                top_staff = sorted(staff, key=lambda x: x["count"], reverse=True)[:5]
-                top_staff = [
-                    {
-                        "staff": staff_member["staff"]["name"]["full"],
-                        "count": staff_member["count"],
-                    }
-                    for staff_member in top_staff
-                ]
-                data["mangaStaff"] = top_staff
-
-            # Extract social stats
-            data["socialStats"] = {
-                "totalFollowers": response_data["data"]["followersPage"]["pageInfo"][
-                    "total"
-                ],
-                "totalFollowing": response_data["data"]["followingPage"]["pageInfo"][
-                    "total"
-                ],
-                "totalActivity": sum(
-                    amount["amount"]
-                    for amount in response_data["data"]["User"]["stats"][
-                        "activityHistory"
-                    ]
-                ),
-                "threadPostsCommentsCount": response_data["data"]["threadsPage"][
-                    "pageInfo"
-                ]["total"]
-                + response_data["data"]["threadCommentsPage"]["pageInfo"]["total"],
-                "totalReviews": response_data["data"]["reviewsPage"]["pageInfo"][
-                    "total"
-                ],
-            }
-            log_message(f"Successfully fetched data for {username}", "info")
+            try:
+                data.update(fetch_anime_data(response_data, keys))
+                data.update(fetch_manga_data(response_data, keys))
+                data.update(fetch_social_data(response_data))
+            except Exception as e:  # pylint: disable=W0703
+                log_message(f"Error: {e}", "error")
+                return None
         except requests.exceptions.RequestException as error:
             if error.response and error.response.status_code == 429:
                 log_message("Rate limit exceeded", "error")
                 return None
             raise error
-
         return data
     except requests.exceptions.RequestException as error:
         log_message(f"Failed to fetch data for user {username}: {error}", "error")
