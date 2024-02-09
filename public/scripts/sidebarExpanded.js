0875fa271eeaff32b50a35b9d538d0b7660d73d0
RLAlpha49 <75044176+RLAlpha49@users.noreply.github.com>
2024-02-08 23:49:22 -0600

    Refactor sidebarExpanded.js to adjust footer width based only on AniCards page

--- public/scripts/sidebarExpanded.js
+++ public/scripts/sidebarExpanded.js
@@ -2,21 +2,20 @@
 
 // Add a click event listener to the element with id 'arrow'
 document.getElementById('arrow').addEventListener('click', function () {
-    // Get the first element with class 'sidebar'
     var sidebar = document.getElementsByClassName('sidebar')[0];
-    // Get the first element with class 'footer'
     var footer = document.getElementsByClassName('footer-div')[0];
 
-    // Check if the sidebar has the class 'expanded'
     if (sidebar.classList.contains('expanded')) {
-        // If it does, remove the 'expanded' class
         sidebar.classList.remove('expanded');
-        // Adjust the width of the footer
-        footer.style.width = 'calc(100% - 62px)';
+        // Check if the current page is 'anicards.html'
+        if (window.location.pathname === '/AniCards/') {
+            footer.style.width = 'calc(100% - 62px)';
+        }
     } else {
-        // If it doesn't, add the 'expanded' class
         sidebar.classList.add('expanded');
-        // Adjust the width of the footer
-        footer.style.width = 'calc(100% - 180px)';
+        // Check if the current page is 'anicards.html'
+        if (window.location.pathname === '/AniCards/') {
+            footer.style.width = 'calc(100% - 180px)';
+        }
     }
 });
