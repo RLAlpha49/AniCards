cea6117e5d52663c77517c12de182ea3a1e29ff3
RLAlpha49 <75044176+RLAlpha49@users.noreply.github.com>
2024-02-12 23:02:22 -0600

    Fromat svgButton.js

--- public/scripts/svgButton.js
+++ public/scripts/svgButton.js
@@ -5,7 +5,7 @@
 window.onload = function () {
   // The keys and username variables will be defined in the HTML file
   // Loop over each key and fetch the corresponding SVG
-  for (let key of keys) {
+  for (const key of keys) {
     fetchSvg(key, username)()
   }
 }
@@ -19,7 +19,7 @@ function fetchSvg(key, username) {
 
     // Fetch the SVG from the server
     fetch(url)
-      .then((response) => {
+      .then(response => {
         // If the response status is 404, throw an error indicating the SVG was not found
         if (response.status === 404) {
           throw new Error('SVG not found')
@@ -31,25 +31,23 @@ function fetchSvg(key, username) {
         // Otherwise, return the response text
         return response.text()
       })
-      .then((svg) => {
+      .then(svg => {
         // Get the container element by its ID, which is assumed to be the same as the key
         const container = document.getElementById(key)
 
         // Set the inner HTML of the container to an image element with the SVG as its source
         // The SVG is base64-encoded and prefixed with the necessary data URL metadata
         // Add an alt attribute to the image element
-        container.innerHTML = `<img src='data:image/svg+xmlbase64,${btoa(
-          svg
-        )}' alt='SVG image of ${key}' />`
+        container.innerHTML = `<img src="data:image/svg+xml;base64,${btoa(svg)}" alt="SVG image of ${key}" />`
       })
-      .catch((error) => {
+      .catch(error => {
         // Get the container element by its ID, which is assumed to be the same as the key
         const container = document.getElementById(key)
 
         // If the error message indicates the SVG was not found
         if (error.message === 'SVG not found') {
           // Set the inner HTML of the container to a button that can be clicked to retry fetching the SVG
-          container.innerHTML = `<button class='svg-button' onclick='(fetchSvg('${key}', '${username}'))()'>Retry creating SVG</button>`
+          container.innerHTML = `<button class="svg-button" onclick="(fetchSvg('${key}', '${username}'))()">Retry creating SVG</button>`
         } else {
           // Otherwise, set the inner HTML of the container to an error message
           container.innerHTML = `Error: ${error.message}`
