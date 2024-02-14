25fcf0b449d9cd517723109a510ea51c8a9a65fd
RLAlpha49 <75044176+RLAlpha49@users.noreply.github.com>
2024-02-14 10:57:19 -0600

    Add sidebar arrow and change transition speed

--- public/scripts/sidebarExpanded.js
+++ public/scripts/sidebarExpanded.js
@@ -4,15 +4,18 @@
 document.getElementById('arrow').addEventListener('click', function () {
   const sidebar = document.getElementsByClassName('sidebar')[0]
   const footer = document.getElementsByClassName('footer-div')[0]
+  const arrow = document.getElementById('arrow')
 
   if (sidebar.classList.contains('expanded')) {
     sidebar.classList.remove('expanded')
+    arrow.classList.remove('expanded')
     // Check if the current page is 'anicards.html'
     if (window.location.pathname === '/') {
       footer.style.width = 'calc(100% - 62px)'
     }
   } else {
     sidebar.classList.add('expanded')
+    arrow.classList.add('expanded')
     // Check if the current page is 'anicards.html'
     if (window.location.pathname === '/') {
       footer.style.width = 'calc(100% - 180px)'
