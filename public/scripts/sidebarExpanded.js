bec146ee7944656719376aaa09e4fe459e39dc4b
RLAlpha49 <75044176+RLAlpha49@users.noreply.github.com>
2024-02-26 09:19:01 -0600

    Format js and css

--- public/scripts/sidebarExpanded.js
+++ public/scripts/sidebarExpanded.js
@@ -12,37 +12,37 @@ document.getElementById('arrow').addEventListener('click', function () {
     // Check if the current page is 'anicards.html'
     if (window.location.pathname === '/') {
       if (window.innerWidth < 400) {
-        footer.style.width = 'calc(100% - ' + (64 * 0.5) + 'px)';
+        footer.style.width = 'calc(100% - ' + (64 * 0.5) + 'px)'
       } else if (window.innerWidth < 450) {
-        footer.style.width = 'calc(100% - ' + (64 * 0.6) + 'px)';
+        footer.style.width = 'calc(100% - ' + (64 * 0.6) + 'px)'
       } else if (window.innerWidth < 500) {
-        footer.style.width = 'calc(100% - ' + (64 * 0.7) + 'px)';
+        footer.style.width = 'calc(100% - ' + (64 * 0.7) + 'px)'
       } else if (window.innerWidth < 550) {
-        footer.style.width = 'calc(100% - ' + (64 * 0.8) + 'px)';
+        footer.style.width = 'calc(100% - ' + (64 * 0.8) + 'px)'
       } else if (window.innerWidth < 700) {
-        footer.style.width = 'calc(100% - ' + (64 * 0.9) + 'px)';
+        footer.style.width = 'calc(100% - ' + (64 * 0.9) + 'px)'
       } else {
-        footer.style.width = 'calc(100% - 64px)';
+        footer.style.width = 'calc(100% - 64px)'
       }
     }
-    } else {
-      sidebar.classList.add('expanded')
-      arrow.classList.add('expanded')
-      // Check if the current page is 'anicards.html'
-      if (window.location.pathname === '/') {
-        if (window.innerWidth < 400) {
-          footer.style.width = 'calc(100% - ' + (160 * 0.5) + 'px)';
-        } else if (window.innerWidth < 450) {
-          footer.style.width = 'calc(100% - ' + (160 * 0.6) + 'px)';
-        } else if (window.innerWidth < 500) {
-          footer.style.width = 'calc(100% - ' + (160 * 0.7) + 'px)';
-        } else if (window.innerWidth < 550) {
-          footer.style.width = 'calc(100% - ' + (160 * 0.8) + 'px)';
-        } else if (window.innerWidth < 700) {
-          footer.style.width = 'calc(100% - ' + (160 * 0.9) + 'px)';
-        } else {
-          footer.style.width = 'calc(100% - 160px)';
-        }
+  } else {
+    sidebar.classList.add('expanded')
+    arrow.classList.add('expanded')
+    // Check if the current page is 'anicards.html'
+    if (window.location.pathname === '/') {
+      if (window.innerWidth < 400) {
+        footer.style.width = 'calc(100% - ' + (160 * 0.5) + 'px)'
+      } else if (window.innerWidth < 450) {
+        footer.style.width = 'calc(100% - ' + (160 * 0.6) + 'px)'
+      } else if (window.innerWidth < 500) {
+        footer.style.width = 'calc(100% - ' + (160 * 0.7) + 'px)'
+      } else if (window.innerWidth < 550) {
+        footer.style.width = 'calc(100% - ' + (160 * 0.8) + 'px)'
+      } else if (window.innerWidth < 700) {
+        footer.style.width = 'calc(100% - ' + (160 * 0.9) + 'px)'
+      } else {
+        footer.style.width = 'calc(100% - 160px)'
       }
     }
+  }
 })
