4aa1e36287b12850b2538617202588fd5142120d
RLAlpha49 <75044176+RLAlpha49@users.noreply.github.com>
2024-02-26 09:13:47 -0600

    Completely scaled home page for smaller viewports & Fixed header not always following scroll on users page

--- public/scripts/sidebarExpanded.js
+++ public/scripts/sidebarExpanded.js
@@ -11,14 +11,38 @@ document.getElementById('arrow').addEventListener('click', function () {
     arrow.classList.remove('expanded')
     // Check if the current page is 'anicards.html'
     if (window.location.pathname === '/') {
-      footer.style.width = 'calc(100% - 62px)'
+      if (window.innerWidth < 400) {
+        footer.style.width = 'calc(100% - ' + (64 * 0.5) + 'px)';
+      } else if (window.innerWidth < 450) {
+        footer.style.width = 'calc(100% - ' + (64 * 0.6) + 'px)';
+      } else if (window.innerWidth < 500) {
+        footer.style.width = 'calc(100% - ' + (64 * 0.7) + 'px)';
+      } else if (window.innerWidth < 550) {
+        footer.style.width = 'calc(100% - ' + (64 * 0.8) + 'px)';
+      } else if (window.innerWidth < 700) {
+        footer.style.width = 'calc(100% - ' + (64 * 0.9) + 'px)';
+      } else {
+        footer.style.width = 'calc(100% - 64px)';
+      }
     }
-  } else {
-    sidebar.classList.add('expanded')
-    arrow.classList.add('expanded')
-    // Check if the current page is 'anicards.html'
-    if (window.location.pathname === '/') {
-      footer.style.width = 'calc(100% - 180px)'
+    } else {
+      sidebar.classList.add('expanded')
+      arrow.classList.add('expanded')
+      // Check if the current page is 'anicards.html'
+      if (window.location.pathname === '/') {
+        if (window.innerWidth < 400) {
+          footer.style.width = 'calc(100% - ' + (160 * 0.5) + 'px)';
+        } else if (window.innerWidth < 450) {
+          footer.style.width = 'calc(100% - ' + (160 * 0.6) + 'px)';
+        } else if (window.innerWidth < 500) {
+          footer.style.width = 'calc(100% - ' + (160 * 0.7) + 'px)';
+        } else if (window.innerWidth < 550) {
+          footer.style.width = 'calc(100% - ' + (160 * 0.8) + 'px)';
+        } else if (window.innerWidth < 700) {
+          footer.style.width = 'calc(100% - ' + (160 * 0.9) + 'px)';
+        } else {
+          footer.style.width = 'calc(100% - 160px)';
+        }
+      }
     }
-  }
 })
