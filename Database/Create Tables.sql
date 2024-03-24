511de1a736de61ece2d323eef1a9c930aa46d537
RLAlpha49 <75044176+RLAlpha49@users.noreply.github.com>
2024-03-24 16:19:28 -0500

    Fix sqlfluff linting errors

--- Database/Create Tables.sql
+++ Database/Create Tables.sql
@@ -7,71 +7,71 @@ CREATE TABLE svg.users (
 CREATE TABLE svg.statcards (
     id SERIAL PRIMARY KEY,
     keys VARCHAR(255),
-    user_id INTEGER NOT NULL UNIQUE REFERENCES svg.users(userId)
+    user_id INTEGER NOT NULL UNIQUE REFERENCES svg.users (userId)
 );
 
 CREATE TABLE svg.animeStats (
     id SERIAL PRIMARY KEY,
     data TEXT,
-    user_id INTEGER NOT NULL REFERENCES svg.statcards(user_id)
+    user_id INTEGER NOT NULL REFERENCES svg.statcards (user_id)
 );
 
 CREATE TABLE svg.socialStats (
     id SERIAL PRIMARY KEY,
     data TEXT,
-    user_id INTEGER NOT NULL REFERENCES svg.statcards(user_id)
+    user_id INTEGER NOT NULL REFERENCES svg.statcards (user_id)
 );
 
 CREATE TABLE svg.mangaStats (
     id SERIAL PRIMARY KEY,
     data TEXT,
-    user_id INTEGER NOT NULL REFERENCES svg.statcards(user_id)
+    user_id INTEGER NOT NULL REFERENCES svg.statcards (user_id)
 );
 
 CREATE TABLE svg.animeGenres (
     id SERIAL PRIMARY KEY,
     data TEXT,
-    user_id INTEGER NOT NULL REFERENCES svg.statcards(user_id)
+    user_id INTEGER NOT NULL REFERENCES svg.statcards (user_id)
 );
 
 CREATE TABLE svg.animeTags (
     id SERIAL PRIMARY KEY,
     data TEXT,
-    user_id INTEGER NOT NULL REFERENCES svg.statcards(user_id)
+    user_id INTEGER NOT NULL REFERENCES svg.statcards (user_id)
 );
 
 CREATE TABLE svg.animeVoiceActors (
     id SERIAL PRIMARY KEY,
     data TEXT,
-    user_id INTEGER NOT NULL REFERENCES svg.statcards(user_id)
+    user_id INTEGER NOT NULL REFERENCES svg.statcards (user_id)
 );
 
 CREATE TABLE svg.animeStudios (
     id SERIAL PRIMARY KEY,
     data TEXT,
-    user_id INTEGER NOT NULL REFERENCES svg.statcards(user_id)
+    user_id INTEGER NOT NULL REFERENCES svg.statcards (user_id)
 );
 
 CREATE TABLE svg.animeStaff (
     id SERIAL PRIMARY KEY,
     data TEXT,
-    user_id INTEGER NOT NULL REFERENCES svg.statcards(user_id)
+    user_id INTEGER NOT NULL REFERENCES svg.statcards (user_id)
 );
 
 CREATE TABLE svg.mangaGenres (
     id SERIAL PRIMARY KEY,
     data TEXT,
-    user_id INTEGER NOT NULL REFERENCES svg.statcards(user_id)
+    user_id INTEGER NOT NULL REFERENCES svg.statcards (user_id)
 );
 
 CREATE TABLE svg.mangaTags (
     id SERIAL PRIMARY KEY,
     data TEXT,
-    user_id INTEGER NOT NULL REFERENCES svg.statcards(user_id)
+    user_id INTEGER NOT NULL REFERENCES svg.statcards (user_id)
 );
 
 CREATE TABLE svg.mangaStaff (
     id SERIAL PRIMARY KEY,
     data TEXT,
-    user_id INTEGER NOT NULL REFERENCES svg.statcards(user_id)
+    user_id INTEGER NOT NULL REFERENCES svg.statcards (user_id)
 );
