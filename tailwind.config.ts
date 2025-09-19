60a759c3e0dca3db9b9a4de6238384cbca9bbcfd
RLAlpha49 <75044176+RLAlpha49@users.noreply.github.com>
2025-09-18 23:54:42 -0500

    Migrate from next lint to ESlint

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import animate from "tailwindcss-animate";
 
 const config = {
   darkMode: ["class"],
@@ -99,7 +100,7 @@ const config = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [animate],
 } satisfies Config;
 
 export default config;
