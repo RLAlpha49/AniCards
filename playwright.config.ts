c9b6dcbf47d8695e6f47088aba7516e4581021ec
RLAlpha49 <75044176+RLAlpha49@users.noreply.github.com>
2025-12-08 22:16:25 -0600

    fix(config): update worker configuration for CI to use percentage

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : 5,
+  workers: process.env.CI ? "50%" : 5,
   reporter: [
     ["list"],
     ["html", { outputFolder: "./tests/e2e/playwright-report", open: "never" }],
