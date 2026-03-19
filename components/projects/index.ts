3b00a1c21445b15f16ffb14d5cdd94dc0c008b88
RLAlpha49 <75044176+RLAlpha49@users.noreply.github.com>
2026-03-19 17:49:18 -0500

    feat(projects): restructure projects components and add new features

---
+++ components/projects/index.ts
@@ -0,0 +1,6 @@
+export { FeaturedProject } from "./FeaturedProject";
+export { ProjectCard } from "./ProjectCard";
+export { ProjectCollection } from "./ProjectCollection";
+export { ProjectEthos } from "./ProjectEthos";
+export { ProjectsCTA } from "./ProjectsCTA";
+export { ProjectsHeroSection } from "./ProjectsHeroSection";
